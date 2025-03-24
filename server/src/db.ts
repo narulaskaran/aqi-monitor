d61e8e6afebad1832faa2cbe4128c38172c534da
Karan <ksn@narula.xyz>
2025-03-24 19:19:43 -0400

    prisma generate

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,6 +1,14 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
+}
 
 export async function addSubscription(phone: string, zipCode: string) {
   return await prisma.userSubscription.create({
