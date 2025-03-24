b8c72eed5c6c03d1279191e066d9d372d18d25c1
Karan <ksn@narula.xyz>
2025-03-24 19:27:42 -0400

    pls work

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: true,
+    emptyOutDir: true
   },
   server: {
     proxy: {
@@ -14,6 +16,11 @@ export default defineConfig({
         target: 'http://localhost:3000',
         changeOrigin: true
       }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
     }
   }
 }) 
